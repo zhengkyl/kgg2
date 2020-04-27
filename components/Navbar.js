8eae6639a8b4c497b5350fa706a93f0632e74a97
Kyle Zheng <kylezheng73@gmail.com>
2020-04-27 05:29:20 -0400

    complete response navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import { useRouter } from 'next/router'
 import SunIcon from "@material-ui/icons/Brightness5";
 import MoonIcon from "@material-ui/icons/NightsStay";
 import {
   AppBar,
-  Button,
   Drawer,
+  Divider,
   IconButton,
   Toolbar,
   List,
   ListItem,
-  ListItemText,
+  ListItemIcon,
   Slide,
   useScrollTrigger,
   Typography,
@@ -17,37 +18,39 @@ import {
 } from "@material-ui/core";
 import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import kggLogo from "/img/kgg_black.png";
 import AnimatedMenuIcon from "./AnimatedMenuIcon";
-
+import KggLogo from "../public/img/kgg_logo.svg";
+import clsx from 'clsx'
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: 100,
-  },
   appBar: {
     zIndex: 1400,
   },
   drawer: {},
-  list: {padding:0},
+  list: {
+    padding: 0,
+    display: "inline-block",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+  },
   listItem: {
-    // flexDirection:'row',
-    // paddingLeft: 36,
-    // paddingRight: 36,
-    // paddingTop:16,
-    // paddingBottom:16,
-    // textAlign:'center',
-    // width:'120px',
-    [theme.breakpoints.up('md')]:{
-      display: "inline-block",
+    paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
       cursor: "pointer",
       width: "initial",
-      paddingTop:16,
-      paddingBottom:16,
-    }
+      marginRight: theme.spacing(1),
+      marginLeft: theme.spacing(1),
+      paddingTop: 0,
+      paddingBottom: 0,
+      
+    },
+    [theme.breakpoints.up("md")]: {
+      marginRight: theme.spacing(3),
+      marginLeft: theme.spacing(3),
+    },
   },
-  // listText: {
-  //   fontSize: `2rem`,
-  // },
   logo: {
     height: 20,
     width: `auto`,
@@ -55,11 +58,20 @@ const useStyles = makeStyles((theme) => ({
     top: `50%`,
     left: `50%`,
     transform: `translate(-50%, -50%)`,
+    fill: theme.palette.text.primary,
+    [theme.breakpoints.up("sm")]: {
+      left: 0,
+      transform: 'translate(50%, -50%)',
+    },
   },
   link: {
+    fontSize:'1.25rem',
+    fontWeight:300,
     color: "black",
     height: "100%",
     textDecoration: `none`,
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
     "&:hover": {
       textDecoration: `underline`,
     },
@@ -70,15 +82,30 @@ const useStyles = makeStyles((theme) => ({
       color: "inherit",
     },
   },
+  activeLink: {
+    borderBottom: '2px solid blue',
+
+  },
   toolbar: {
-    flexDirection:'row-reverse',
+    flexDirection: "row-reverse",
+    alignItems: 'stretch',
+  },
+  icon: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+  },
+  iconButton:{
+    marginTop:'auto',
+    marginBottom:'auto',
+  },
+  divider: {
+    margin: `${theme.spacing(1)}px 0`,
   }
 }));
 
 const pages = [
-  // { title: 'Home', path: '/'},
+  { title: 'About', path: '/'},
   { title: "News", path: "/news" },
-  { title: "Blog", path: "/blog" },
   { title: "Social", path: "/social" },
   { title: "Store", path: "/store" },
 ];
@@ -97,9 +124,7 @@ function HideOnScroll(props) {
 export default function Navbar(props) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  const router=useRouter();
 
   const toggleDrawer = (open) => (event) => {
     if (
@@ -111,30 +136,42 @@ export default function Navbar(props) {
     setOpen(open);
   };
 
-  const list = () => (
+  const mobileList = () => (
     <List className={classes.list}>
       {pages.map((page) => (
         <Link href={page.path}>
           <ListItem button key={page.title} className={classes.listItem}>
-            <ListItemText className={classes.listText}>
+            <Typography variant="h5" component="a">
               {page.title}
-            </ListItemText>
+            </Typography>
           </ListItem>
         </Link>
       ))}
+      <Divider className={classes.divider}/>
+      <ListItem button key="toggle-dark" onClick={props.toggleTheme}>
+        <Typography variant="subtitle1">
+          {props.darkMode ? "Dark Mode" : "Light Mode"}
+        </Typography>
+        <ListItemIcon className={classes.icon}>
+          {props.darkMode ? <MoonIcon></MoonIcon> : <SunIcon></SunIcon>}
+        </ListItemIcon>
+      </ListItem>
     </List>
   );
-  const list2 = () => (
+  const desktopList = () => (
     <List className={classes.list}>
       {pages.map((page) => (
         <ListItem key={page.title} className={classes.listItem}>
           <Link href={page.path}>
-            <a title={page.title} className={classes.link}>
+            <a title={page.title} className={clsx(classes.link, router.pathname === page.path && classes.activeLink)}>
               {page.title}
             </a>
           </Link>
         </ListItem>
       ))}
+        <IconButton onClick={props.toggleTheme} edge="end" className={classes.iconButton}>
+          {props.darkMode ? <MoonIcon></MoonIcon> : <SunIcon></SunIcon>}
+        </IconButton>
     </List>
   );
 
@@ -148,45 +185,30 @@ export default function Navbar(props) {
           className={classes.appBar}
         >
           <Toolbar className={classes.toolbar}>
-            <Hidden mdUp>
-              <IconButton onClick={toggleDrawer(!open)} edge="start">
-              <AnimatedMenuIcon open={open}></AnimatedMenuIcon>
-            </IconButton>
+            <Hidden smUp>
+              <IconButton onClick={toggleDrawer(!open)} edge="end" className={classes.iconButton}>
+                <AnimatedMenuIcon open={open} ></AnimatedMenuIcon>
+              </IconButton>
             </Hidden>
-            
-            {props.darkMode ? (
-              <img src="/img/kgg_white.png" className={classes.logo} />
-            ) : (
-              <img src="/img/kgg_black.png" className={classes.logo} />
-            )}
-            <IconButton onClick={props.toggleTheme}>
-              {props.darkMode ? <MoonIcon></MoonIcon> : <SunIcon></SunIcon>}
-            </IconButton>
-            {/* {pages.map((page) => (
-              <Link href={page.path}>
-                <a title={page.title} className={classes.link}>
-                  {page.title}
-                </a>
-              </Link>
-            ))} */}
-            <Hidden smDown>
-              {list2()}
-            </Hidden>
-            
+            <Hidden xsDown>{desktopList()}</Hidden>
+            <KggLogo className={classes.logo} />
           </Toolbar>
         </AppBar>
       </HideOnScroll>
-      <Hidden mdUp>
+      <Hidden smUp>
         <Drawer
-        className={classes.drawer}
-        anchor="right"
-        open={open}
-        onClose={toggleDrawer(false)}
-      >
-        {list()}
-      </Drawer>
+          className={classes.drawer}
+          anchor="right"
+          open={open}
+          onClose={toggleDrawer(false)}
+        >
+          <Toolbar />
+          {mobileList()}
+        </Drawer>
       </Hidden>
-      
+
+      {/* Spacing element to pad top of page with height of appbar */}
+      <Toolbar></Toolbar>
     </>
   );
 }
